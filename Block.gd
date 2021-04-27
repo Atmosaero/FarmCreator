ae2e836c39fbb54818ab0fcb9912b2173a247466
Atmosaero <41229804+Atmosaero@users.noreply.github.com>
2021-04-27 11:42:42 +0300

    Version 2

--- Block.gd
+++ Block.gd
@@ -4,18 +4,19 @@ extends MeshInstance
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var material
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var material = self.get_node(".").get_surface_material(0)
+	material.albedo_color = Color(1, 0, 0)
 
 func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
 	print("cool!")
-	self.build()
+	build()
 		
 func build():
-	self.modulate = Color(0,0,1)
+	set_surface_material(0, material)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
