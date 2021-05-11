9730190080e7f395be050c68342017468879ef66
Atmosaero <41229804+Atmosaero@users.noreply.github.com>
2021-05-11 21:51:53 +0300

    Version 9

--- Block.gd
+++ Block.gd
@@ -5,16 +5,14 @@ var material = SpatialMaterial.new()
 export(Material) var textureWater
 export(Material) var textureGrass
 export(Material) var textureDirt
-
+export(Material) var textureAsphalt
 
 func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			print("BrushID", BaseDate.BrushID)
 			if BaseDate.BrushID == 1:#Red
-				material.albedo_color = Color(1, 0, 0)
-				 
-				set_surface_material(0, material)
+				set_surface_material(0, textureAsphalt)
 				print("BrushID", BaseDate.BrushID)
 			if BaseDate.BrushID == 2:#Green
 				#material.albedo_color = Color(0, 1, 0, 1)
@@ -26,6 +24,3 @@ func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx
 			if BaseDate.BrushID == 4:#Brown
 				set_surface_material(0, textureDirt)	
 				print("BrushID", BaseDate.BrushID)
-	
-func _ready():
-	pass
